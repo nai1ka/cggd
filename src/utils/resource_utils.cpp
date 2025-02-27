5b15a74e658889b14e211961bad176119031cd5f
nai1ka <nail.minnemullin@mail.ru>
2025-02-27 15:52:55 +0300

    lab1 v1

--- src/utils/resource_utils.cpp
+++ src/utils/resource_utils.cpp
@@ -8,8 +8,6 @@
 #include <stb_image_write.h>
 
 
-using namespace cg::utils;
-
 std::string view_command(const std::filesystem::path& path)
 {
 #ifdef __APPLE__
@@ -21,10 +19,10 @@ std::string view_command(const std::filesystem::path& path)
 	return "";
 }
 
-void cg::utils::save_resource(cg::resource<cg::unsigned_color>& render_target, std::filesystem::path filepath)
+void cg::utils::save_resource(cg::resource<cg::unsigned_color>& render_target, const std::filesystem::path filepath)
 {
 	int width = static_cast<int>(render_target.get_stride());
-	int height = static_cast<int>(render_target.count()) / width;
+	int height = static_cast<int>(render_target.get_number_of_elements()) / width;
 
 	int result = stbi_write_png(
 			filepath.string().c_str(), width, height, 3, render_target.get_data(),
@@ -37,4 +35,3 @@ void cg::utils::save_resource(cg::resource<cg::unsigned_color>& render_target, s
 	if (!command.empty())
 		std::system(command.c_str());
 }
-
